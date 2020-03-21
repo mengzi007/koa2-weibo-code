c91798ca9adb4f428b8cfb93fc32e9ddfa7248c4
liaomeng <liaomeng@hotmail.com>
2020-03-21 22:24:56 +0800

    commit 2

--- src/conf/db.js
+++ src/conf/db.js
@@ -3,37 +3,37 @@
  * @author neil liao
  */
 
- const { isProd } = require('../utils/env')
+const { isProd } = require('../utils/env')
 
- let REDIS_CONF = {
-     port: 6379,
-     host: '127.0.0.1'
- }
+let REDIS_CONF = {
+  port: 6379,
+  host: '127.0.0.1'
+}
 
- let MYSQL_CONF = {
-     host: 'localhost',
-     user: 'root',
-     password: 'P@ssw0rd',
-     port: '3306',
-     database: 'koa2_weibo_db'
- }
+let MYSQL_CONF = {
+  host: 'localhost',
+  user: 'root',
+  password: 'P@ssw0rd',
+  port: '3306',
+  database: 'koa2_weibo_db'
+}
 
- if (isProd) {
-     REDIS_CONF = {
-         port: 6397,
-         host: '127.0.0.1'
-     }
+if (isProd) {
+  REDIS_CONF = {
+    port: 6397,
+    host: '127.0.0.1'
+  }
 
-     MYSQL_CONF = {
-        host: 'localhost',
-        user: 'root',
-        password: 'P@ssw0rd',
-        port: '3306',
-        database: 'koa2_weibo_db'
-    }
- }
+  MYSQL_CONF = {
+    host: 'localhost',
+    user: 'root',
+    password: 'P@ssw0rd',
+    port: '3306',
+    database: 'koa2_weibo_db'
+  }
+}
 
- module.exports = {
-    REDIS_CONF,
-    MYSQL_CONF
- }
+module.exports = {
+  REDIS_CONF,
+  MYSQL_CONF
+}
--- src/db/seq.js
+++ src/db/seq.js
@@ -10,22 +10,22 @@ const { isProd, isTest } = require('../utils/env')
 const { host, user, password, database } = MYSQL_CONF
 
 const conf = {
-    host,
-    dialect: 'mysql'
+  host,
+  dialect: 'mysql'
 }
 
 //如果是单元测试，那么关掉sql打印
 if(isTest) {
-    conf.logging = () => {}
+  conf.logging = () => {}
 }
 
 //线上环境，使用连接池
-if(isProd) {
-    conf.pool = {
-        max: 5, //连接池中最大连接数
-        min: 0, //最小
-        idle: 10000 //如果一个连接池 10 秒之内没有被使用，则释放
-    }
+if (isProd) {
+  conf.pool = {
+    max: 5, //连接池中最大连接数
+    min: 0, //最小
+    idle: 10000 //如果一个连接池 10 秒之内没有被使用，则释放
+  };
 }
 
 
